493de4427a56c2aef9471fce0a821db15eebc3b8
Sai <155558419+SaiNonSai@users.noreply.github.com>
2025-07-30 02:14:13 +0100

    basic done

---
+++ Assets/Scripts/Enemy/EnemyContactDamage.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyContactDamage : MonoBehaviour
+{
+    public int damageAmount = 10;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth player = other.GetComponent<PlayerHealth>();
+            if (player != null)
+            {
+                player.TakeDamage(damageAmount);
+            }
+
+            Destroy(gameObject); 
+        }
+    }
+}
